c1921b0d423590f3515624bfecc5f0544c25a943
Admiralfe <felix.liu@hotmail.se>
2018-03-20 16:41:14 +0100

    Nytt projekt inklusive .gitignore

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+	public GameObject Point;
+	
+	//Set this in Unity UI
+	public int numberOfAgents;
+	// Use this for initialization
+	private void Start () 
+	{
+		for (int i = 0; i < numberOfAgents; i++)
+		{
+			Instantiate(Point, new Vector3(Random.value * 5, Random.value * 5, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+}
---
+++ Assets/Script/MovingPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPoint : MonoBehaviour
+{
+	public Vector2 velocity;
+
+	// Use this for initialization
+	void Start()
+	{
+		velocity = Random.insideUnitCircle * 0.01f;
+	}
+
+	void Update()
+	{
+		transform.Translate(velocity);	
+	}
+}
